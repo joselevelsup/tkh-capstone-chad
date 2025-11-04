40a661ee128de4aeb48f3c7bc8bd796204a79e8c
Chad <chadzhang4@gmail.com>
2025-11-03 21:02:23 -0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,25 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
+function App() {
+  const [count, setCount] = useState(0)
 
-
+  return (
+     <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/men" element={<MensClothingPage />} />
+        <Route path="/women" element={<WomenClothingPage />} />
+        <Route path="/kids" element={<KidsClothingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+      </Routes>
+    </Router>
+  );
+}
 
 
 
