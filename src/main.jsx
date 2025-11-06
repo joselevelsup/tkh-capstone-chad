b6d6f35dd60fa14c53f947e9568885baf99b4ced
Chad <chadzhang4@gmail.com>
2025-11-05 20:24:36 -0500

    routing setups

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,35 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+
+// Import Routing Components from react-router-dom
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+
+//Import Pages
+import Home from './pages/Home.jsx';
+import MensClothing from './pages/MensClothing.jsx';
+import WomenClothing from './pages/WomenClothing.jsx';
+import KidsClothing from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
+import Contact from './pages/Contact.jsx';
+import About from './pages/About.jsx';
+import Checkout from './pages/Checkout.jsx';
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,19 +39,24 @@ function App() {
   return (
      <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/men" element={<MensClothingPage />} />
-        <Route path="/women" element={<WomenClothingPage />} />
-        <Route path="/kids" element={<KidsClothingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/men" element={<MensClothing />} />
+        <Route path="/women" element={<WomenClothing/>} />
+        <Route path="/kids" element={<KidsClothing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/checkout" element={<Checkout/>} />
       </Routes>
     </Router>
   );
 }
+
+
+
+
+
 
 
 
