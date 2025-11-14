8576b37bd4811905c605d9e76882feac1e5439bb
Chad <chadzhang4@gmail.com>
2025-11-14 16:00:32 -0500

    c

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+    <div>
+
+    </div>
+    )
+}
+
+export default Navbar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,33 @@
+import React from 'react';
 
-export default Home;
-
-import Navbar from './components/Navbar';
-import HeroBanner from './components/HeroBanner';
-import CategoryTeaser from './components/CategoryTeaser';
-import ProductGrid from './components/ProductGrid';
-import Footer from './components/Footer';
-import './App.css'; // Your main styles
-
-function HomePage() {
+function MyButton() {
   return (
-    <div className="homepage">
-      {/* 1. Global Navigation */}
-      <Navbar /> 
-
-      {/* 2. Primary Feature Area */}
-      <HeroBanner 
-        title="Elevate Your Style" 
-        ctaText="Shop New Arrivals" 
-        link="/shop" 
-      />
-
-      {/* 3. Featured Categories */}
-      <section className="featured-categories">
-        <h2>Shop By Category</h2>
-        <CategoryTeaser categoryName="Dresses" imageSrc="" link="/shop/dresses" />
-        <CategoryTeaser categoryName="Outerwear" imageSrc="" link="/shop/outerwear" />
-        {/* ... more teasers */}
-      </section>
-
-      {/* 4. Featured Products */}
-      <section className="featured-products">
-        <h2>Top Sellers</h2>
-        <ProductGrid products={/* array of product objects */} />
-      </section>
-
-      {/* 5. Global Footer */}
-      <Footer />
-    </div>
+    <button>I'm a button</button>
   );
 }
 
-export default HomePage;
+function HomePage(){
+    return(
+        <div>
+        <h1>Welcome to the Home Page!</h1>   
+         <MyButton />
+        <p>This is the main page for our Capstone Clothing Store.</p>
+        </div>
+    );
+}
+
+export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
